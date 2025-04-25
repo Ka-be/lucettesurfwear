dad8a5262357500580674b913d1e02a9b2e1b7b9
ka-be <kevin.bourgitteau@gmail.com>
2025-04-25 10:19:18 +0000

    update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20  sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold uppercase tracking-widest">Lucette Surfwear</h1>
-      <p className="text-lg">T'es pas prêt mon pote</p>
+    <div className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold uppercase tracking-widest text-center">Lucette Surfwear</h1>
+      <p className="text-lg text-center">T'es pas prêt mon pote</p>
     </div>
   );
 }
